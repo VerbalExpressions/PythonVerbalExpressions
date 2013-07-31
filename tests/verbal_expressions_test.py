c98270bbf982e11fbad6d038ead90bc345b56c54
mriehl <maximilien.riehl@gmail.com>
2013-07-31 10:33:22 +0200

    minor test refactorings

--- tests/verbal_expressions_test.py
+++ tests/verbal_expressions_test.py
@@ -7,6 +7,7 @@ class VerExTest(unittest.TestCase):
     '''
         Tests for verbal_expressions.py
     '''
+
     def setUp(self):
         self.v = VerEx()
 
@@ -14,7 +15,7 @@ class VerExTest(unittest.TestCase):
         self.v = None
         self.exp = None
 
-    def test__str__(self):
+    def test_should_render_verex_as_string(self):
         self.assertEquals(str(self.v.add('^$')), '^$')
 
     def test_should_match_characters_in_range(self):
@@ -39,94 +40,98 @@ class VerExTest(unittest.TestCase):
         self.assertNotRegexpMatches('W', self.exp)
 
 
-    def test_start_of_line(self):
+    def test_should_match_start_of_line(self):
         self.exp = self.v.start_of_line().regex()
         self.assertRegexpMatches('text  ', self.exp, 'Not started :(')
 
-    def test_end_of_line(self):
+    def test_should_match_end_of_line(self):
         self.exp = self.v.start_of_line().end_of_line().regex()
         self.assertRegexpMatches('', self.exp, 'It\'s not the end!')
 
-    def test_anything(self):
+    def test_should_match_anything(self):
         self.exp = self.v.start_of_line().anything().end_of_line().regex()
         self.assertRegexpMatches('!@#$%¨&*()__+{}', self.exp, 'Not so anything...')
 
-    def test_anything_but(self):
+    def test_should_match_anything_but_specified_element_when_element_is_not_found(self):
         self.exp = self.v.start_of_line().anything_but('X').end_of_line().regex()
         self.assertRegexpMatches('Y Files', self.exp, 'Found the X!')
 
-    def test_anything_but_false(self):
+    def test_should_not_match_anything_but_specified_element_when_specified_element_is_found(self):
         self.exp = self.v.start_of_line().anything_but('X').end_of_line().regex()
-        self.assertFalse(re.match(self.exp, 'VerEX'), 'Didn\'t found the X :(')
+        self.assertNotRegexpMatches('VerEX', self.exp, 'Didn\'t found the X :(')
 
-    def test_find_true(self):
+    def test_should_find_element(self):
         self.exp = self.v.start_of_line().find('Wally').end_of_line().regex()
         self.assertRegexpMatches('Wally', self.exp, '404! Wally not Found!')
 
-    def test_find_false(self):
+    def test_should_not_find_missing_element(self):
         self.exp = self.v.start_of_line().find('Wally').end_of_line().regex()
-        self.assertFalse(re.match(self.exp, 'Wall-e'), 'DAFUQ is Wall-e?')
+        self.assertNotRegexpMatches('Wall-e', self.exp, 'DAFUQ is Wall-e?')
 
-    def test_maybe(self):
+    def test_should_match_when_maybe_element_is_present(self):
         self.exp = self.v.start_of_line().find('Python2.').maybe('7').end_of_line().regex()
         self.assertRegexpMatches('Python2.7', self.exp, 'Version doesn\'t match!')
 
-    def test_any_true(self):
+    def test_should_match_when_maybe_element_is_missing(self):
+        self.exp = self.v.start_of_line().find('Python2.').maybe('7').end_of_line().regex()
+        self.assertRegexpMatches('Python2.', self.exp, 'Version doesn\'t match!')
+
+    def test_should_match_on_any_when_element_is_found(self):
         self.exp = self.v.start_of_line().any('Q').anything().end_of_line().regex()
         self.assertRegexpMatches('Query', self.exp, 'No match found!')
 
-    def test_any_false(self):
+    def test_should_not_match_on_any_when_element_is_not_found(self):
         self.exp = self.v.start_of_line().any('Q').anything().end_of_line().regex()
-        self.assertFalse(re.match(self.exp, 'W'), 'I\'ve found it!')
+        self.assertNotRegexpMatches('W', self.exp, 'I\'ve found it!')
 
-    def test_line_break_true_n(self):
+    def test_should_match_when_line_break_present(self):
         self.exp = self.v.start_of_line().anything().line_break().anything().end_of_line().regex()
         self.assertRegexpMatches('Marco \n Polo', self.exp, 'Give me a break!!')
 
-    def test_line_break_true_rn(self):
+    def test_should_match_when_line_break_and_carriage_return_present(self):
         self.exp = self.v.start_of_line().anything().line_break().anything().end_of_line().regex()
         self.assertRegexpMatches('Marco \r\n Polo', self.exp, 'Give me a break!!')
 
-    def test_line_break_false(self):
+    def test_should_not_match_when_line_break_is_missing(self):
         self.exp = self.v.start_of_line().anything().line_break().anything().end_of_line().regex()
-        self.assertFalse(re.match(self.exp, 'Marco Polo'), 'There\'s a break here!')
+        self.assertNotRegexpMatches('Marco Polo', self.exp, 'There\'s a break here!')
 
-    def test_tab_true(self):
+    def test_should_match_when_tab_present(self):
         self.exp = self.v.start_of_line().anything().tab().end_of_line().regex()
         self.assertRegexpMatches('One tab only	', self.exp, 'No tab here!')
 
-    def test_tab_false(self):
+    def test_should_not_match_when_tab_is_missing(self):
         self.exp = self.v.start_of_line().anything().tab().end_of_line().regex()
         self.assertFalse(re.match(self.exp, 'No tab here'), 'There\'s a tab here!')
 
-    def test_word_true(self):
+    def test_should_match_when_word_present(self):
         self.exp = self.v.start_of_line().anything().word().end_of_line().regex()
         self.assertRegexpMatches('Oneword', self.exp, 'Not just a word!')
 
-    def test_word_false(self):
+    def test_not_match_when_two_words_are_present_instead_of_one(self):
         self.exp = self.v.start_of_line().anything().tab().end_of_line().regex()
         self.assertFalse(re.match(self.exp, 'Two words'), 'I\'ve found two of them')
 
-    def test_or_true(self):
+    def test_should_match_when_or_condition_fulfilled(self):
         self.exp = self.v.start_of_line().anything().find('G').OR().find('h').end_of_line().regex()
         self.assertRegexpMatches('Github', self.exp, 'Octocat not found')
 
-    def test_or_false(self):
+    def test_should_not_match_when_or_condition_not_fulfilled(self):
         self.exp = self.v.start_of_line().anything().find('G').OR().find('h').end_of_line().regex()
         self.assertFalse(re.match(self.exp, 'Bitbucket'), 'Bucket not found')
 
-    def test_any_case(self):
+    def test_should_match_on_upper_case_when_lower_case_is_given_and_any_case_is_true(self):
         self.exp = self.v.start_of_line().find('THOR').end_of_line().with_any_case(True).regex()
         self.assertRegexpMatches('thor', self.exp, 'Upper case Thor, please!')
 
-    def test_multi_line(self):
+    def test_should_match_multiple_lines(self):
         self.exp = self.v.start_of_line().anything().find('Pong').anything().end_of_line().search_one_line(True).regex()
         self.assertRegexpMatches('Ping \n Pong \n Ping', self.exp, 'Pong didn\'t answer')
 
-    def test_email(self):
+    def test_should_match_email_address(self):
         self.exp = self.v.start_of_line().word().then('@').word().then('.').word().end_of_line().regex()
         self.assertRegexpMatches('mail@mail.com', self.exp, 'Not a valid email')
 
-    def test_url(self):
+    def test_should_match_url(self):
         self.exp = self.v.start_of_line().then('http').maybe('s').then('://').maybe('www.').word().then('.').word().maybe('/').end_of_line().regex()
         self.assertRegexpMatches('https://www.google.com/', self.exp, 'Not a valid email')
