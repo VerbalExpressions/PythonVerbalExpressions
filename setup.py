e523246e554447987c408575c0a04383aa549f19
Victor Titor <vtitor.edumix@gmail.com>
2018-01-08 03:05:12 +0200

    Update setup.py for new release

--- setup.py
+++ setup.py
@@ -4,24 +4,29 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(
-          name = 'VerbalExpressions',
-          version = '0.0.1',
-          description = 'Make difficult regular expressions easy! Python port of the awesome VerbalExpressions repo - https://github.com/jehna/VerbalExpressions',
-          long_description = '''Please see https://github.com/VerbalExpressions/PythonVerbalExpressions/blob/master/README.md for more information!''',
-          author = "Yan Wenjun, diogobeda, Mihai Ionut Vilcu, Peder Soholt, Sameer Raghuram, Kharms",
-          author_email = "",
-          license = 'MIT',
-          url = 'https://github.com/VerbalExpressions/PythonVerbalExpressions',
-          test_suite='tests',
-          scripts = [],
-          packages = ['verbalexpressions'],
-          tests_require = ['six'],
-          classifiers = [
-                          'License :: OSI Approved :: MIT License',
-                          'Programming Language :: Python',
-                          'Topic :: Software Development :: Libraries',
-                          'Topic :: Text Processing'
-                        ],
-          zip_safe=True
+        name='VerbalExpressions',
+        version='0.0.2',
+        description='Make difficult regular expressions easy! Python port of the awesome VerbalExpressions repo - https://github.com/jehna/VerbalExpressions',
+        long_description='''Please see https://github.com/VerbalExpressions/PythonVerbalExpressions/blob/master/README.md for more information!''',
+        author='Victor Titor, Yan Wenjun, diogobeda, Mihai Ionut Vilcu, Peder Soholt, Sameer Raghuram, Kharms',
+        author_email='',
+        license='MIT',
+        url='https://github.com/VerbalExpressions/PythonVerbalExpressions',
+        test_suite='tests',
+        scripts=[],
+        packages=['verbalexpressions'],
+        tests_require=['six'],
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Text Processing'
+        ],
+        zip_safe=True
     )
-
