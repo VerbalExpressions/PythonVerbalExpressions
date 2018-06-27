65daeaa08720bc05c82a38e92ff07fc4bc78eb19
Victor Titor <vtitor.edumix@gmail.com>
2018-06-27 12:45:02 +0300

    Add py37 support

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries",
         "Topic :: Text Processing",
     ],
