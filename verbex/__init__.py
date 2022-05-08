82d23555912bb5b2e14cdea6d524d2c1a1c36563
rbroderi <richard@.sanguinesoftware.com>
2022-05-08 17:23:55 -0400

    added auto doc generate pre-commit hook

--- verbex/__init__.py
+++ verbex/__init__.py
@@ -1,3 +1,7 @@
+import importlib.metadata
+
 from .verbex import CharClass as CharClass
 from .verbex import SpecialChar as SpecialChar
 from .verbex import Verbex as Verbex
+
+__version__ = importlib.metadata.version("verbex")
